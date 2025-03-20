b437647e0f3214c12d278b243105e04ab69db78f
Abdoulaye223 <abdoulayeisimaga@gmail.com>
2025-03-20 03:56:17 +0000

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import {Lusitana} from 'next/font/google';
+import { preload } from 'react-dom';
  
 export const inter = Inter({ subsets: ["latin"] });
-export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"], preload: false });
+export const lusitana = Lusitana({ weight:"400", preload: false});
